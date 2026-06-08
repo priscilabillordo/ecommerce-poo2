e2ef9a43861b0a86bb6f0ade600181974d4ff937
Priscila <priscilanbillordo@gmail.com>
2026-06-08 16:33:18 -0300

    Agrego atributo a Producto

--- src/main/java/Producto.java
+++ src/main/java/Producto.java
@@ -1,2 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Producto {
+
+// Prueba Producto
+    private String nombre;
+
+    public Producto(String nombre){
+        this.nombre = nombre;
+    }
 }
