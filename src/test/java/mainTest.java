787e927746bbce908a42958bfd3047a24e68003a
priscilabillordo <priscilabillordounq@gmail.com>
2026-07-13 20:10:38 -0300

    Completo Main

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,5 +1,6 @@
 import ecommerce.EcommerceData;
 import formato.Formato;
+import formato.FormatoCSV;
 import formato.FormatoHTML;
 import formato.FormatoTXT;
 import item.Item;
@@ -21,10 +22,11 @@ import reporte.ReporteProductosMasVendidos;
 import unqShop.UnqShop;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class mainTest {
     private UnqShop unqShop;
@@ -48,14 +50,14 @@ public class mainTest {
     private MetodoDeEnvio envioEstandar;
     private MetodoDeEnvio envioExpress;
 
-    private Sucursal sucuralCentral;
+    private Sucursal sucursalCentral;
     private EnvioExpressI   envioExpressI;
     private CorreoArgentino correoArgentino;
     private APITransferencia  apiTransferencia;
     private APIBilletera      apiBilletera;
     private APITarjetaCredito apiTarjetaCredito;
 
-    private Item productoAudiculares;
+    private Item productoAuriculares;
     private Item productoTeclado;
     private Item productoLaptop;
     private Item productoCamara;
@@ -63,7 +65,7 @@ public class mainTest {
 
     @BeforeEach
     void setUp() {
-        productoAudiculares = new Producto(
+        productoAuriculares = new Producto(
                 "Audicular Sony",
                 "Audiculares con un diseño de lujo elaborado con materiales cuidadosamente seleccionados",
                 0.30,
@@ -89,7 +91,7 @@ public class mainTest {
         productoLaptop  = new Producto(
                 "Laptop Lenovo ThinkPad",
                 "Notebook de alto rendimiento con procesador Intel Core i7, 16 GB de RAM y SSD de 512 GB",
-                1.75,
+                0,
                 "SKU003",
                 "Lenovo",
                 "Tecnologia",
@@ -111,15 +113,17 @@ public class mainTest {
 
         paqueteGamer = new Paquete(
                 "Oferta de gamer",
-                "Incluye un teclado y un audicular",
+                "Incluye un teclado y un auricular",
                 0.25
         );
+        paqueteGamer.incluir(productoTeclado);
+        paqueteGamer.incluir(productoAuriculares);
 
         ecommerceData = new EcommerceData();
         unqShop = new UnqShop(ecommerceData);
 
         //SE AGREGAN LOS ITEMS AL UNQSHOP
-        unqShop.agregarItem(productoAudiculares);
+        unqShop.agregarItem(productoAuriculares);
         unqShop.agregarItem(productoTeclado);
         unqShop.agregarItem(productoCamara);
         unqShop.agregarItem(productoLaptop);
@@ -127,16 +131,19 @@ public class mainTest {
         unqShop.agregarItem(paqueteGamer);
 
         //EXTRAS
-        sucuralCentral  = mock(Sucursal.class);
+        sucursalCentral = mock(Sucursal.class);
+        when(sucursalCentral.hayStock(any())).thenReturn(true);
         correoArgentino = mock(CorreoArgentino.class);
+        when(correoArgentino.estimarEnvio(anyFloat(), anyString())).thenReturn(2000f);
         envioExpressI   = mock(EnvioExpressI.class);
+        when(envioExpressI.calcularCosto(anyFloat())).thenReturn(1000f);
 
         apiTarjetaCredito = mock(APITarjetaCredito.class);
         apiTransferencia  = mock(APITransferencia.class);
         apiBilletera = mock(APIBilletera.class);
 
         //TIPOS DE ENVIOS
-        retiroSucursal  = new RetiroEnSucursal(sucuralCentral);
+        retiroSucursal  = new RetiroEnSucursal(sucursalCentral);
         envioEstandar   = new EnvioEstandar(correoArgentino);
         envioExpress    = new EnvioExpress(envioExpressI);
 
@@ -144,11 +151,6 @@ public class mainTest {
         transferenciaBancaria = new TransferenciaBancaria("1234567891011121314151","priscilaCuenta", apiTransferencia);
         billeteraVirtual = new BilleteraVirtual(20000, apiBilletera);
         tarjetaCredito   = new TarjetaDeCredito("1234-5678-7654-3210", "100", LocalDate.of(2030,6,29), apiTarjetaCredito);
-
-        //reprote de venta
-        // 4 pedidos
-        // ventas 2
-        // pedido cancelado
 
         //PEDIDOS; 2 PEDIDO ENVIADO, 1 PEDIDO CANCELADO, 1 PEDIDO EN PROCESO
         unPedidoEnviado   = new Pedido("Av. Siempre viva", transferenciaBancaria, retiroSucursal, ecommerceData);
@@ -162,19 +164,54 @@ public class mainTest {
         unqShop.registrarPedido(pedidoCancelado);
         unqShop.registrarPedido(pedidoEnProceso);
 
+        // =======================================  FLUJO DE PEDIDOS  ===============================================
+
+        // PEDIDO ENVIADO 1
+        unPedidoEnviado.agregarItem(paqueteGamer);
+        unPedidoEnviado.confirmar();
+        unPedidoEnviado.preparar();
+        unPedidoEnviado.enviar();
+        unPedidoEnviado.entregar();
+
+        // PEDIDO ENVIADO 2
+        otroPedidoEnviado.agregarItem(paqueteGamer);
+        otroPedidoEnviado.agregarItem(productoLaptop);
+        otroPedidoEnviado.confirmar();
+        otroPedidoEnviado.preparar();
+        otroPedidoEnviado.enviar();
+        otroPedidoEnviado.entregar();
+
+        // PEDIDO CANCELADO CON REEMBOLSO
+        pedidoCancelado.agregarItem(productoLaptop);
+        pedidoCancelado.agregarItem(productoAuriculares);
+        pedidoCancelado.confirmar();
+        pedidoCancelado.preparar();
+        pedidoCancelado.cancelar();
+
+
+        // PEDIDO EN PROCESO
+        pedidoEnProceso.agregarItem(paqueteGamer);
+        pedidoEnProceso.sacarItem(paqueteGamer);
+        pedidoEnProceso.agregarItem(productoAuriculares);
 
         //REPORTE
-        reporteDeVendidos = new ReporteProductosMasVendidos(LocalDate.of(2026,7,13), LocalDate.of(2026,7,20));
+        reporteDeVendidos = new ReporteProductosMasVendidos(LocalDate.of(2026,7,13), LocalDate.now());
 
         //TIPOS DE FORMATO
-        formatocsv  = new FormatoTXT();
+        formatocsv  = new FormatoCSV();
         formatohtml = new FormatoHTML();
         formatotxt  = new FormatoTXT();
     }
 
     @Test
     void verificar() {
-        unqShop.generarReporte(reporteDeVendidos, formatocsv);
+        System.out.println("Sistema de Gestion UNQ SHOP");
+        System.out.println("Los reportes de las ventas son:");
+        System.out.println(unqShop.generarReporte(reporteDeVendidos, formatocsv));
+        System.out.println(unqShop.generarReporte(reporteDeVendidos, formatotxt));
+        System.out.println("La cantidad de pedidos es " + unqShop.getPedidos().size());
+        System.out.println("La cantidad de ventas es " + unqShop.ventas().size());
+        System.out.println("La cantidad de notas de credito es " + unqShop.notasDeCredito().size());
     }
 
 }
